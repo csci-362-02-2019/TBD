b1b1af096924e5e02a2d6239afe046d51b56e4b8
setzlerte <tsetzler3@gmail.com>
2019-11-07 11:48:13 -0500

    power functions and test cases

---
+++ TestAutomation/scripts/TestScript/test_drivers/powTest.py
@@ -0,0 +1,34 @@
+from sugar_methods import functions
+from sugar_methods import rational
+from util import utilities
+
+def runPowTest(test_case):
+	print(test_case["test_discription"])
+	test_id = test_case["id"]
+	test_name = test_case["test_name"]
+	description = test_case["test_discription"]
+	inputs = test_case["input"]
+	oracle = test_case["oracle"]
+	x = test_case["input"][0]
+	y = test_case["input"][1]
+
+	try:
+		output = str(functions.pow(x,y))
+	except Exception as e:
+		output = str(e)
+	
+	
+		
+	
+	if (output == oracle):
+		print("Test Passed {} = {}".format(oracle, output))
+		inputs = ('%s, %s'%( x , y))
+		utilities.outputToFile(str(test_id), test_name, description, inputs, oracle, output, 'Passed', '../../temp/results.txt')
+	else:
+		inputs = ('%s, %s'%( x, y))
+		print("Test Failed {} = {},".format(oracle, output))
+		utilities.outputToFile(str(test_id), test_name, description, inputs, oracle, output, 'Failed', '../../temp/results.txt')
+
+	print("\n \n")
+	
+	
